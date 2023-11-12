bdc237d0f70abfdbadfcee3a676c530a1ab5a2c9
kimsy1106 <schwarzesloch1106@gmail.com>
2023-11-13 00:14:16 +0900

    update

--- tcp-server.py
+++ tcp-server.py
@@ -17,7 +17,7 @@ while 1:
      print('Client address:', addr)
      data = conn.recv(BUFFER_SIZE)
      #if not data: break
-     currentTime = " " + " 201902664 김성윤 updated !!! " + time.ctime(time.time()) + "\r\n"
+     currentTime = " " + " 201902664 김성윤?? updated !!! " + time.ctime(time.time()) + "\r\n"
      print(data.decode('utf-8'))
      data = data + currentTime.encode('ascii')
      conn.send(data)  # echo
